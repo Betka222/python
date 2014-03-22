7ab308d1e3eeef860524cfcf6219041576c742d8
David Roesel <roesel@gmail.com>
2014-03-22 12:49:53 +0100

    oprava monte carlo, zbývá okomentovat
    možná to nebude fungovat bez canopy (scipy)

--- navrh/montecarlo.py
+++ navrh/montecarlo.py
@@ -1,3 +1,5 @@
+from __future__ import division
+from scipy import integrate
 import random
 import math
 
@@ -8,19 +10,25 @@ def funkce(x):
 
 def monte(xmin, xmax, ymin, ymax, N):
     zasah = 0
-    S= abs(xmax-xmin)*abs(ymax-xmin)
-    hod=0
-    while hod<N:
-        for i in range (1,N):
-            x= random.uniform(xmin, xmax)
-            y= random.uniform(ymin, ymax)
-            
-            hod+=1
-            if  y < math.sin(x):
-                zasah+=1
+    S = abs(xmax-xmin)*abs(ymax-xmin)
+    
+    for i in range (1,N):
+        x= random.uniform(xmin, xmax)
+        y= random.uniform(ymin, ymax)
+        
+        if  y < funkce(x):
+            zasah+=1
+    
     obsah = (zasah/N)*S
     return obsah
-    
-print("Funkce sin(x) na intervalu (0,pi) X (0,1) uzavira pod krivkou: ") 
-print(monte(0, pi, 0, 1, 100));
 
+xmin = 0
+xmax = pi
+ymin = 0
+ymax = 1
+    
+print("Funkce sin(x) na intervalu (0,pi) X (0,1) uzavira podle MC: ") 
+print(monte(xmin, xmax, ymin, ymax, 100));
+
+print("\nFunkce sin(x) na intervalu (0,pi) X (0,1) uzavira pod krivkou: ") 
+print(integrate.quad(lambda x: funkce(x), 0, pi)[0])
